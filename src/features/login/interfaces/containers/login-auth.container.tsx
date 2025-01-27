18174397bb129ca87ded936e3183deda5999314b
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-27 13:22:21 -0300

    feat: code

--- src/features/login/interfaces/containers/login-auth.container.tsx
+++ src/features/login/interfaces/containers/login-auth.container.tsx
@@ -1,6 +1,6 @@
-import { JSX } from 'react'
+import { JSX, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { LogIn } from 'lucide-react'
+import { Eye, EyeOff, LogIn } from 'lucide-react'
 
 import { Button, Input, Label } from '@app/shared/components'
 
@@ -8,10 +8,14 @@ import { Button, Input, Label } from '@app/shared/components'
  * Login auth container.
  */
 export const LoginAuth = (): JSX.Element => {
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handleTogglePasswordVisibility = () => setShowPassword(!showPassword)
+
   return (
     <div className="w-full max-w-[500px] rounded-lg bg-white shadow-xl">
       <div className="flex items-center justify-center border-b border-border px-8 py-6 text-center md:px-12">
-        <h1 className="text-xl font-bold md:text-2xl">Sistema de gestión de rebates</h1>
+        <h1 className="text-xl font-semibold md:text-2xl">Sistema de gestión de rebates</h1>
       </div>
 
       <div className="flex flex-col items-center justify-center gap-2 px-8 py-6 text-center md:px-12">
@@ -28,12 +32,25 @@ export const LoginAuth = (): JSX.Element => {
         <div className="flex flex-col gap-1">
           <Label htmlFor="password">Contraseña</Label>
 
-          <Input
-            className="max-md:h-10 max-md:px-4"
-            id="password"
-            placeholder="Ingresa tu contraseña"
-            type="password"
-          />
+          <div className="relative">
+            <Input
+              className="max-md:h-10 max-md:px-4"
+              id="password"
+              placeholder="Ingresa tu contraseña"
+              type={showPassword ? 'text' : 'password'}
+            />
+
+            <Button
+              aria-label={showPassword ? 'Ocultar contraseña' : 'Mostrar contraseña'}
+              className="absolute right-0 top-0 h-full hover:bg-transparent hover:shadow-none"
+              onClick={handleTogglePasswordVisibility}
+              size="icon"
+              type="button"
+              variant="text"
+            >
+              {showPassword ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
+            </Button>
+          </div>
         </div>
       </form>
 
