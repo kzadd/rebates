4088eb85c7984dd99951c44a99d59922f1e69a16
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-28 14:27:53 -0300

    feat: code

--- src/layouts/brand.layout.tsx
+++ src/layouts/brand.layout.tsx
@@ -21,7 +21,7 @@ export const Brand = (): JSX.Element => {
         {isOpenSidebar && <div className="fixed inset-0 z-40 bg-black opacity-50" onClick={handleCloseSidebar} />}
         <Sidebar isOpenSidebar={isOpenSidebar} setIsOpenSidebar={setIsOpenSidebar} />
 
-        <div className="flex flex-1 flex-col pl-[var(--sidebar-icon-width)]">
+        <div className="flex flex-1 pl-[var(--sidebar-icon-width)]">
           <main className="flex-1 overflow-y-auto p-4">
             <Outlet />
           </main>
