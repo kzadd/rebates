c45d6bb747a501d106e34ecfc55c0043b7215e75
Ricardo Carrasquero <rjcarrasquero@ticmoai.com>
2025-01-29 00:56:41 -0300

    feat: code

--- src/features/login/application/useLoginAuth.ts
+++ src/features/login/application/useLoginAuth.ts
@@ -4,20 +4,26 @@ import { useNavigate } from 'react-router-dom'
 import { FULL_ROUTE_PATHS, TOKEN_KEYS } from '@app/shared/constants/app.constant'
 import { putCookie } from '@app/shared/utils/cookie.utils'
 
+interface LoginAuthControls {
+  isShowPassword: boolean
+  onSingIn: () => void
+  onTogglePasswordVisibility: () => void
+}
+
 /**
  * Hook to manage the state and functionalities of the login auth.
  */
-export const useLoginAuth = () => {
+export const useLoginAuth = (): LoginAuthControls => {
   const navigate = useNavigate()
 
-  const [isShowPassword, setIsShowPassword] = useState(false)
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false)
 
-  const handleSingIn = useCallback((): void => {
+  const handleSingIn = useCallback(() => {
     putCookie(TOKEN_KEYS.accessToken, 'testing')
     navigate(FULL_ROUTE_PATHS.dashboard.root)
   }, [navigate])
 
-  const handleTogglePasswordVisibility = useCallback((): void => {
+  const handleTogglePasswordVisibility = useCallback(() => {
     setIsShowPassword(prev => !prev)
   }, [])
 
